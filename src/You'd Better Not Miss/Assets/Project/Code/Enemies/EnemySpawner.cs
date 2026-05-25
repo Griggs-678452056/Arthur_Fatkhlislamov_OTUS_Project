b3acddc5f378067b12a3e166298be230a57481e3
Artur Fatkhlislamov <arturfathlislamov@gmail.com>
2026-05-25 23:43:37 +0500

    Update(disactivate enemy pool)

--- src/You'd Better Not Miss/Assets/Project/Code/Enemies/EnemySpawner.cs
+++ src/You'd Better Not Miss/Assets/Project/Code/Enemies/EnemySpawner.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 namespace Code
 {
-    public class EnemySpawner : MonoBehaviour
+    public class EnemySpawner : MonoBehaviour // убрать пул врагов, скрипт EnemyPool сохранить на всякий случай
     {
         [SerializeField] private EnemyPool _enemyPool;
         [SerializeField] private Transform[] _spawnPoints;
